9b0f119d088e4c8a461a75df482ed81e6c3c3ff7
Carl Singleton <Kiyocarl@gmail.com>
2019-01-31 01:17:23 -0500

    can't install node module

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+
+
+USE note_db;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  item_id VARCHAR(10) NOT NULL,
+  product_name VARCHAR (100) NOT NULL,
+  department_name VARCHAR (01) NOT NULL,
+  price INT default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE note_db;
+
+INSERT INTO notes
+ (title, note)
+VALUES
+ ('');
+
+INSERT INTO notes
+ (title, note)
+VALUES
+ ('');
